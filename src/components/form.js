fe87c1c73d687bb4301da1aa791c1a09ab025b1b
mikhaelkamdem <56977804+mikhaelkamdem@users.noreply.github.com>
2022-06-13 14:57:06 +0100

    Update form.js

--- src/components/form.js
+++ src/components/form.js
@@ -9,9 +9,9 @@ import Title from "./title"
 
 
 function Form({selectedImage, setSelectedImage, ipfs, loading, setLoading}) {
-    const [addressValue, setAddressValue] = useState('4PGC6SU4YL4JVQIOX4CQSYFSAF5VLKUUPP4KC47ZK25I3QJDBBBB3PTV6A')
+    const [addressValue, setAddressValue] = useState('4PGC6SU4YL4JVQIOX4CQSYFSAF5VLKUUPP4KC47ZK25I3QJDBBBB3PTV6C')
     const [addressError, setAddressError] = useState(false)
-    const [skValue, setSKValue] = useState('[30, 104, 54, 27, 124, 129, 82, 206, 12, 139, 126, 61, 205, 2, 192, 111, 210, 181, 77, 68, 248, 0, 93, 152, 118, 138, 194, 249, 70, 241, 203, 11, 227, 204, 47, 74, 156, 194, 248, 154, 193, 14, 191, 5, 9, 96, 178, 1, 123, 85, 170, 148, 123, 248, 161, 115, 249, 86, 186, 141, 193, 35, 8, 66]')
+    const [skValue, setSKValue] = useState('[30, 104, 54, 27, 134, 128, 82, 206, 12, 139, 126, 62, 205, 2, 192, 111, 210, 181, 77, 68, 248, 0, 93, 152, 118, 138, 194, 249, 70, 241, 203, 11, 227, 204, 47, 74, 156, 194, 248, 154, 193, 14, 191, 5, 9, 96, 178, 1, 123, 85, 170, 148, 123, 248, 161, 115, 249, 86, 186, 141, 193, 35, 8, 66]')
     const [skError, setSKError] = useState(false)
     const [totalInsuanceValue, setTotalInsuanceValue] = useState('1')
     const [totalInsuanceError, setTotalInsuanceError] = useState(false)
@@ -19,7 +19,7 @@ function Form({selectedImage, setSelectedImage, ipfs, loading, setLoading}) {
     const [unitNameError, setUnitNameError] = useState(false)
     const [assetNameValue, setAssetNameValue] = useState('Test')
     const [assetNameError, setAssetNameError] = useState(false)
-    const [managerValue, setManagerValue] = useState('4PGC6SU4YL4JVQIOX4CQSYFSAF5VLKUUPP4KC47ZK25I3QJDBBBB3PTV6A')
+    const [managerValue, setManagerValue] = useState('4PGC6SU4YL4JVQIOX4CQSYFSAF5VLKUUPP4KC47ZK25I3QJDBBBB3PTV6C')
     const [managerError, setManagerError] = useState(false)
     const [feeValue, setFeeValue] = useState('10000')
     const [feeError, setFeeError] = useState(false)
